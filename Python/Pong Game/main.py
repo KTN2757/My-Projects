601e250efde25af11c3c48e3199f25d797c1fe46
KTN__ <kritagyadahal588@gmail.com>
2023-07-17 15:36:16 +0545

    Displaying the Window
    added a while loop

--- Python/Pong Game/main.py
+++ Python/Pong Game/main.py
@@ -1,8 +1,13 @@
+# Importing Libraries
 import random
 import pygame
+
+# Initializing pygame
 pygame.init()
 
-#Window
+# Window
+w, h = 800, 600
+window = display.setmode((800, 600))
 running = True
 while running:
-    
+    for even in pygame.event.get():
