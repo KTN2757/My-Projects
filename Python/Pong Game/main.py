3a1ff670797a1d4b32c2c0d008302df6213c4ad3
KTN__ <kritagyadahal588@gmail.com>
2023-07-17 16:13:39 +0545

    Rackets Added
    Added 2 lines in perfect location to act as a player/bot/racket. Also used pygame as py

--- Python/Pong Game/main.py
+++ Python/Pong Game/main.py
@@ -1,13 +1,23 @@
 # Importing Libraries
 import random
-import pygame
+import pygame as py
 
 # Initializing pygame
-pygame.init()
+py.init()
 
 # Window
 w, h = 800, 600
-window = display.setmode((800, 600))
+window = py.display.set_mode((800, 600))
 running = True
+py.display.set_caption("Pong Game")
+window.fill((0, 0, 0))
+
+line1 = py.draw.line(window, (255, 255, 255), (5, h//2), (5, h//2+50), 12)
+line2 = py.draw.line(window, (255, 255, 255), (793, h//2), (793, h//2+50), 12)
+
+# Game Loop
 while running:
-    for even in pygame.event.get():
+    for event in py.event.get():
+        if event.type == py.QUIT:
+            running = False
+    py.display.update()
