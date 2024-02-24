26acbe9b4336b83a1cdf3a828dc8f02bfc7d039e
KTN2757 <kritagyadahal588@gmail.com>
2024-02-24 17:39:04 +0545

    Added racket movement

--- Python/Pong Game/main.py
+++ Python/Pong Game/main.py
@@ -2,14 +2,14 @@
 import random
 import pygame as py
 import time
-import numpy as np
+import math
 
 # Initializing pygame
 py.init()
 
 # Window
 w, h = 800, 600
-window = py.display.set_mode((800, 600))
+window = py.display.set_mode((w, h))
 running = True
 py.display.set_caption("Pong Game")
 gameOverFont = py.font.SysFont("Comic Sans MS.ttf", 64)
@@ -38,26 +38,24 @@ def draw_ball(x, y):
 racket1X1, racket1Y1 = 5, h // 2
 racket1X2, racket1Y2 = 10, 50
 
-racket2X1, racket2Y1 = 785, h // 2
+racket2X1, racket2Y1 = 785, 450  # h//2
 racket2X2, racket2Y2 = 10, 50
 
 ballX = w // 2
 ballY = h // 2
 
-ballXSpeed = 0
-ballYSpeed = 0
+ballXSpeed = 0.3
+ballYSpeed = 0.3
 
 
-def racket2MoveUp(x1, y1, x2, y2):
-    pass
+def racket2MoveUp(y1):
+    y1 -= 0.5
+    return y1
 
 
-def racket2MoveDown():
-    racket2StartY -= 10
-    racket2EndY -= 10
-
-
-# Collision
+def racket2MoveDown(y1):
+    y1 += 0.5
+    return y1
 
 
 # Game Over
@@ -81,12 +79,6 @@ while running:
         if event.type == py.QUIT:
             running = False
         if event.type == py.KEYDOWN:
-            if event.key == py.K_SPACE:
-                pass
-            if event.key == py.K_UP:
-                racket2MoveUp(racket2X1, racket2Y1, racket2X2, racket2Y2)
-            if event.key == py.K_DOWN:
-                pass
             if gameIsOver == True and event.key == py.K_r:
                 ballX = w // 2
                 ballY = h // 2
@@ -94,13 +86,31 @@ while running:
                 running = False
 
     window.fill((0, 0, 0))
+
+    # Racket Control
+    keys = py.key.get_pressed()
+    if keys[py.K_UP]:
+        racket2Y1 = racket2MoveUp(racket2Y1)
+    if keys[py.K_DOWN]:
+        racket2Y1 = racket2MoveDown(racket2Y1)
+
+    # Movement
     draw_ball(ballX, ballY)
     draw_racket1(racket1X1, racket1Y1, racket1X2, racket1Y2)
     draw_racket2(racket2X1, racket2Y1, racket2X2, racket2Y2)
     ballX += ballXSpeed
     ballY += ballYSpeed
-    if ballX <= 0 or ballX >= w or ballY <= 0 or ballY >= h:
+
+    # Check Collision & Boundary
+    if ballX <= 0 or ballY <= 0:
+        ballYSpeed = -ballYSpeed
+    if ballX >= w:
         gameOver(ballX, ballY)
         gameIsOver = True
-    # time.sleep(0.5)
+    if ballY >= h:
+        ballYSpeed = -ballYSpeed
+    dist = math.sqrt((ballX - racket2X1) ** 2 + (ballY - racket2Y1) ** 2)
+    if dist <= 48:
+        ballXSpeed = -ballXSpeed
+        ballYSpeed = -ballYSpeed
     py.display.update()
